c98e3226346151dd2b908758d6332b9fb275abbd
a-abramow <abramow0000@yandex.ru>
2021-02-05 13:50:41 +0300

    1

--- ping-pong.py
+++ ping-pong.py
@@ -3,7 +3,7 @@ from random import choice, randint
 
 window = turtle.Screen()
 window.title("Ping-pong")
-window.setup(width=1.0,height=1.0)
+window.setup(width=1.0, height=1.0)
 window.bgcolor("black")
 window.tracer(2)
 
@@ -11,18 +11,18 @@ border = turtle.Turtle()
 border.speed(0)
 border.color('green')
 border.begin_fill()
-border.goto(-500,300)
-border.goto(500,300)
-border.goto(500,-300)
-border.goto(-500,-300)
-border.goto(-500,300)
+border.goto(-500, 300)
+border.goto(500, 300)
+border.goto(500, -300)
+border.goto(-500, -300)
+border.goto(-500, 300)
 border.end_fill()
 
-border.goto(0,300)
+border.goto(0, 300)
 border.color('white')
 border.setheading(270)
 for i in range(25):
-    if i%2==0:
+    if i % 2 == 0:
         border.forward(24)
     else:
         border.up()
@@ -30,13 +30,14 @@ for i in range(25):
         border.down()
 border.hideturtle()
 
-#rocket_a
+# rocket_a
 rocket_a = turtle.Turtle()
 rocket_a.color('white')
 rocket_a.shape('square')
-rocket_a.shapesize(stretch_len=1,stretch_wid=5)
+rocket_a.shapesize(stretch_len=1, stretch_wid=5)
 rocket_a.penup()
-rocket_a.goto(-450,0)
+rocket_a.goto(-450, 0)
+
 
 def move_up():
     y = rocket_a.ycor() + 50
@@ -44,19 +45,21 @@ def move_up():
         y = 250
     rocket_a.sety(y)
 
+
 def move_down():
     y = rocket_a.ycor() - 50
     if y < -250:
         y = -250
     rocket_a.sety(y)
 
-#rocket_b
+
+# rocket_b
 rocket_b = turtle.Turtle()
 rocket_b.color('white')
 rocket_b.shape('square')
-rocket_b.shapesize(stretch_len=1,stretch_wid=5)
+rocket_b.shapesize(stretch_len=1, stretch_wid=5)
 rocket_b.penup()
-rocket_b.goto(450,0)
+rocket_b.goto(450, 0)
 
 FONT = ("Arial", 30)
 score_a = 0
@@ -64,7 +67,7 @@ s_1 = turtle.Turtle(visible=False)
 s_1.color('white')
 s_1.shape()
 s_1.penup()
-s_1.setposition(-250,300)
+s_1.setposition(-250, 300)
 s_1.write(score_a, font=FONT)
 
 score_b = 0
@@ -72,8 +75,9 @@ s_2 = turtle.Turtle(visible=False)
 s_2.color('white')
 s_2.shape()
 s_2.penup()
-s_2.setposition(250,300)
+s_2.setposition(250, 300)
 s_2.write(score_b, font=FONT)
+
 
 def move_up_2():
     y = rocket_b.ycor() + 10
@@ -81,11 +85,13 @@ def move_up_2():
         y = 250
     rocket_b.sety(y)
 
+
 def move_down_2():
     y = rocket_b.ycor() - 10
     if y < -250:
         y = -250
     rocket_b.sety(y)
+
 
 window.listen()
 window.onkeypress(move_up, 'w')
@@ -112,31 +118,29 @@ while True:
 
     if ball.ycor() <= -290:
         ball.dy = -ball.dy
-    
+
     if ball.xcor() >= 490:
         score_b += 1
         s_2.clear()
         s_2.write(score_b, font=FONT)
-        ball.goto(0,randint(-150,150))
-        ball.dx = choice([-4,-3,-2,2,3,4])
-        ball.dy = choice([-4,-3,-2,2,3,4])
+        ball.goto(0, randint(-150, 150))
+        ball.dx = choice([-4, -3, -2, 2, 3, 4])
+        ball.dy = choice([-4, -3, -2, 2, 3, 4])
 
     if ball.xcor() <= -490:
         score_a += 1
         s_1.clear()
         s_1.write(score_a, font=FONT)
-        ball.goto(0,randint(-150,150))
-        ball.dx = choice([-4,-3,-2,2,3,4])
-        ball.dy = choice([-4,-3,-2,2,3,4])
+        ball.goto(0, randint(-150, 150))
+        ball.dx = choice([-4, -3, -2, 2, 3, 4])
+        ball.dy = choice([-4, -3, -2, 2, 3, 4])
 
-    if ball.ycor() >= rocket_b.ycor()-50 and ball.ycor() <= rocket_b.ycor()+50\
-        and ball.xcor() >= rocket_b.xcor()-5 and ball.xcor() <= rocket_b.xcor()+5:
-        ball.dx = -ball.dx
-    
-    if ball.ycor() >= rocket_a.ycor()-50 and ball.ycor() <= rocket_a.ycor()+50\
-        and ball.xcor() >= rocket_a.xcor()-5 and ball.xcor() <= rocket_a.xcor()+5:
+    if ball.ycor() >= rocket_b.ycor() - 50 and ball.ycor() <= rocket_b.ycor() + 50 \
+            and ball.xcor() >= rocket_b.xcor() - 5 and ball.xcor() <= rocket_b.xcor() + 5:
         ball.dx = -ball.dx
 
-
+    if ball.ycor() >= rocket_a.ycor() - 50 and ball.ycor() <= rocket_a.ycor() + 50 \
+            and ball.xcor() >= rocket_a.xcor() - 5 and ball.xcor() <= rocket_a.xcor() + 5:
+        ball.dx = -ball.dx
 
 window.mainloop()
